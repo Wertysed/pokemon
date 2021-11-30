e6fdec386e14f614add555a3ef1a89c3a6a6638a
Wertysed <wertysed11@gmail.com>
2021-11-30 21:28:19 +0500

    1 вроде сделано 2 вроде сделано

--- File1.py
+++ File1.py
@@ -1,7 +1,13 @@
 import requests
 from collections.abc import Iterator
+from typing import Union
+from typing import Any
 
 url = 'https://pokeapi.co/api/v2/pokemon/132'
+
+
+class PokeError(Exception):
+    pass
 
 
 class BasePokemon:
@@ -16,16 +22,15 @@ class BasePokemon:
 
 
 class Pokemon(BasePokemon):
-    def __init__(self, abobus):
+    def __init__(self, abobus: Any):
         BasePokemon.__init__(self, abobus)
         self.__id = abobus['id']
         # self.__name = abobus['name']
         self.__height = abobus['height']
         self.__weight = abobus['weight']
 
-    def __gt__(self, other):
+    def __gt__(self, other: Any):
         return self.__weight > other.__weight
-
 
     def __str__(self):
         return f'id {self.__id} ' \
@@ -53,38 +58,53 @@ class Pokemon(BasePokemon):
                    f'name {self._name} ' \
                    f'height {self.__height} ' \
                    f'weight {self.__weight} '
-        raise ValueError
+        raise PokeError('introduced something incorrect')
 
 
 class PokemonAPI:
+
     @staticmethod
-    def get_pokemon(argument):
-        url_1 = f'https://pokeapi.co/api/v2/pokemon/{argument}'
-        bibus = requests.get(url_1).json()
-        return Pokemon(bibus)
+    def get_pokemon(argument: Union[int, str]):
+
+        if not isinstance(argument, (int, str)):
+            raise PokeError('introduced something incorrect, only str or int')
+
+        elif isinstance(argument, (int, str)):
+            url_1 = f'https://pokeapi.co/api/v2/pokemon/{argument}'
+            bibus = requests.get(url_1).json()
+            return Pokemon(bibus)
 
     @staticmethod
     def get_all(get_full: bool = False) -> Iterator:
-        number = 1
-        if not get_full:
+        try:
             number = 1
-            while number < 51:
-                print(number)
-                url_2 = f'https://pokeapi.co/api/v2/pokemon/{number}'
-                bibus_1 = requests.get(url_2).json()
-                yield BasePokemon(bibus_1)
-                number += 1
+            if not get_full:
+                number = 1
+                while number < 50:
+                    try:
+                        print(number)
+                        url_2 = f'https://pokeapi.co/api/v2/pokemon/{number}'
+                        bibus_1 = requests.get(url_2).json()
+                        yield BasePokemon(bibus_1)
+                        number += 1
+                    except PokeError:
+                        print('Pasta')
 
-        else:
-            while number < 50:
-                #print('все должно работать', number)
-                yield PokemonAPI.get_pokemon(number)
-                number += 1
+            else:
+                while number < 50:
+                    # print('все должно работать', number)
+                    yield PokemonAPI.get_pokemon(number)
+                    number += 1
+                    print()
+        except PokeError:
+            print('Some pasta')
 
 
 abobus = requests.get(url).json()
 ditto = Pokemon(abobus)
+print('Задание №1')
 print(ditto)
+
 c = PokemonAPI.get_pokemon(1)
 for i in range(51):
     p = PokemonAPI.get_pokemon(i + 2)
@@ -94,3 +114,9 @@ for i in range(51):
         c = p
 print('самый большой', c)
 
+#g = PokemonAPI.get_all(1231)
+#while True:
+#    print(next(g))
+
+print("реализация исключения")
+f = PokemonAPI.get_pokemon(78.33)
